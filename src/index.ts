75f8620d7de3418bda65243ad4c978cf6da42ad5
Laszlo Szikszai <sziklaszlo@gmail.com>
2022-04-03 23:03:04 +0200

    Simplified types

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,16 @@ import parser, { DEFAULT_OPTIONS, DotEnvParseOptions, ParsedData, ParseResult } 
 
 // const log = debug("dotenv-ng");
 
-export interface DotEnvLoadOptions extends DotEnvParseOptions {
-  overwriteExisting?: boolean;
-}
-
 export const DEFAULT_ENV_FILE = ".env";
 export const LOCAL_ENV_FILE = ".env.local";
 
 
-interface PreparedParameters<O extends DotEnvParseOptions> {
+interface PreparedParameters {
   path: string;
-  options: O;
+  options: DotEnvParseOptions;
 }
 
-function prepareParameters<O extends DotEnvParseOptions>(path?: string | O, options?: O, defaultOptions?: O): PreparedParameters<O> {
+function prepareParameters(path?: string | DotEnvParseOptions, options?: DotEnvParseOptions, defaultOptions?: DotEnvParseOptions): PreparedParameters {
   // call()
   if (!path) {
     return {
@@ -24,7 +20,7 @@ function prepareParameters<O extends DotEnvParseOptions>(path?: string | O, opti
       options: {
         ...DEFAULT_OPTIONS,
         ...(defaultOptions || {}),
-      } as O,
+      },
     };
   }
   // call({options})
@@ -34,8 +30,8 @@ function prepareParameters<O extends DotEnvParseOptions>(path?: string | O, opti
       options: {
         ...DEFAULT_OPTIONS,
         ...(defaultOptions || {}),
-        ...(path as O || {}),
-      } as O,
+        ...(path as DotEnvParseOptions || {}),
+      },
     };
   }
   // call("path")
@@ -46,7 +42,7 @@ function prepareParameters<O extends DotEnvParseOptions>(path?: string | O, opti
       ...DEFAULT_OPTIONS,
       ...(defaultOptions || {}),
       ...(options || {}),
-    } as O,
+    },
   };
 }
 
@@ -58,7 +54,7 @@ export function parseString(content: string, options?: DotEnvParseOptions): Pars
 export function parse(options?: DotEnvParseOptions): ParseResult;
 export function parse(path: string, options?: DotEnvParseOptions): ParseResult;
 export function parse(path?: string | DotEnvParseOptions, options?: DotEnvParseOptions): ParseResult {
-  const { path: parsedPath, options: parsedOptions } = prepareParameters<DotEnvParseOptions>(path, options);
+  const { path: parsedPath, options: parsedOptions } = prepareParameters(path, options);
   // console.log({ parsedPath, parsedOptions });
   parser.setOptions(parsedOptions);
   return parser.parse(parsedPath);
@@ -67,15 +63,15 @@ export function parse(path?: string | DotEnvParseOptions, options?: DotEnvParseO
 export function values<D extends ParsedData>(options?: DotEnvParseOptions): D;
 export function values<D extends ParsedData>(path: string, options?: DotEnvParseOptions): D;
 export function values<D extends ParsedData>(path?: string | DotEnvParseOptions, options?: DotEnvParseOptions): D {
-  const { path: parsedPath, options: parsedOptions } = prepareParameters<DotEnvParseOptions>(path, options);
+  const { path: parsedPath, options: parsedOptions } = prepareParameters(path, options);
   const parsed = parse(parsedPath, parsedOptions);
   return parsed.data as D;
 }
 
-export function load(options?: DotEnvLoadOptions): void;
-export function load(path: string, options?: DotEnvLoadOptions): void;
-export function load(path?: string | DotEnvLoadOptions, options?: DotEnvLoadOptions): void {
-  const { path: parsedPath, options: parsedOptions } = prepareParameters<DotEnvLoadOptions>(path, options);
+export function load(options?: DotEnvParseOptions): void;
+export function load(path: string, options?: DotEnvParseOptions): void;
+export function load(path?: string | DotEnvParseOptions, options?: DotEnvParseOptions): void {
+  const { path: parsedPath, options: parsedOptions } = prepareParameters(path, options);
   const v = values(parsedPath, parsedOptions);
   // @ts-ignore ENV can only contain string, but we can ignore it
   process.env = parsedOptions.overwriteExisting
